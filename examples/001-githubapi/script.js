034255e5a1d53a435d611514288926e1165041b4
Marco <46133335+Marco4413@users.noreply.github.com>
2024-01-18 16:39:04 +0100

    Added Webpage and Examples

---
+++ examples/001-githubapi/script.js
@@ -0,0 +1,34 @@
+import * as iq from "/iq.js";
+
+function CompareISODates(d1, d2) {
+    return new Date(d1).getTime() - new Date(d2).getTime();
+}
+
+async function FetchGitHubCommits(project) {
+    const resp = await fetch(`https://api.github.com/repos/${project}/commits`);
+    return await resp.json();
+}
+
+(async () => {
+    const commits1 = await FetchGitHubCommits("Marco4413/TermVideoPlayer");
+    const commits2 = await FetchGitHubCommits("Marco4413/CP77-DiscordRPC2");
+
+    const query = iq.query()
+        .select("commit")
+        .select(["author", "message"])
+        .where(o => CompareISODates(o.author.date, "2024-01-12") > 0)
+        .take(5)
+        .build();
+
+    console.group("TermVideoPlayer Commits");
+    query.extend()
+        .select("message")
+        .on(commits1)
+        .iforeach(console.log);
+    console.groupEnd();
+
+    console.group("CP77RPC2 Commits");
+    query.on(commits2)
+        .foreach(console.log);
+    console.groupEnd();
+})();
